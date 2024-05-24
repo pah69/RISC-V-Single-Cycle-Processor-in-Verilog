06d41758066caaf07344df32f76f8b9e49ec7e3c
Anh Ho Pham <128297495+pah69@users.noreply.github.com>
2024-05-24 19:50:41 +0700

    Update ALU.sv

--- ALU.sv
+++ ALU.sv
@@ -7,13 +7,13 @@ module ALU(data1, data2, ALU_control, ALU_result, Zero);
 always@(ALU_control, data1, data2) begin
 	case (ALU_control)
 		4'b0000: Zero <= 0; 
-				ALU_result <= data1 && data2;
+				ALU_result <= data1 && data2; // AND
 		4'b0001: Zero <= 0;
-				ALU_result <= data1 || data2;
+				ALU_result <= data1 || data2; // OR
 		4'b0010: Zero <= 0;
-				ALU_result <= data1 + data2;
+				ALU_result <= data1 + data2; // ADD
 		4'b0110: Zero <= 0;
-				ALU_result <= data1 - data2;
+				ALU_result <= data1 - data2; // SUB
 		default :Zero <= 0; ALU_result <= 0;
 	endcase
 end
