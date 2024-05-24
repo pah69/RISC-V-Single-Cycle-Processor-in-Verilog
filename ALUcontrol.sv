32562d8f66763528f488084a99ecf9ea41aef334
Anh Ho Pham <128297495+pah69@users.noreply.github.com>
2024-05-24 19:49:35 +0700

    Update ALUcontrol.sv

--- ALUcontrol.sv
+++ ALUcontrol.sv
@@ -6,12 +6,12 @@ module ALUcontrol(ALUOp, func7, func3, ALUcontrol_out);
 
 	always_comb
 	case ({ALUOp, func7, func3})
-		12'b00_xxxxxxx_xxx: ALUcontrol_out <= 4'b0010;
-		12'bx1_xxxxxxx_xxx: ALUcontrol_out <= 4'b0110;
-		12'b1x_0000000_000: ALUcontrol_out <= 4'b0010;
-		12'b1x_0100000_000: ALUcontrol_out <= 4'b0110;
-		12'b1x_0000000_111: ALUcontrol_out <= 4'b0000;
-		12'b1x_0000000_110: ALUcontrol_out <= 4'b0001;
+		12'b00_xxxxxxx_xxx: ALUcontrol_out <= 4'b0010; // Load or Store - Add 
+ 		12'bx1_xxxxxxx_xxx: ALUcontrol_out <= 4'b0110; // BEQ - subtract
+		12'b1x_0000000_000: ALUcontrol_out <= 4'b0010; // ADD - R type
+		12'b1x_0100000_000: ALUcontrol_out <= 4'b0110; // SUB - R Type
+		12'b1x_0000000_111: ALUcontrol_out <= 4'b0000; // AND- R type
+		12'b1x_0000000_110: ALUcontrol_out <= 4'b0001; // OR - R type
 		default : ALUcontrol_out = 4'b0000;
 	endcase
 		end
